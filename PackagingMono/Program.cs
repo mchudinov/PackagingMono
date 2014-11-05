df14ae9e144486bdc13f1526509d02591a07c828
Mikael Chudinov <mikael@chudinov.net>
2014-11-06 00:11:02 +0100

    Initial commit.

---
+++ PackagingMono/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PackagingMono
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
